978104031b162fc642d302b7df0bfde10953fffa
GBTC408007UR <GBTC408007UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-06-20 10:06:31 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+        String firstInitial = keyboard.next();
+        String lastName = keyboard.next();
+        String houseNumber = keyboard.next();
+        String streetName = keyboard.next();
+        String streetType = keyboard.next();
+        String city = keyboard.next();
+
+
+
+        System.out.print(firstInitial + " " + lastName + " " + houseNumber + " ");
+        System.out.println(streetName + " " + streetType + " " + city);
+    }
+}
